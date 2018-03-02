a9b9978120fbf6e4141936c2d21acb6308428d2b
Jonathan Campo <joacampora@unal.edu.co>
2018-03-02 17:51:13 -0500

    The Hero Editor Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms'; // <= NgModel, para las inputs - Enlace bidireccional
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [    // Lista de modulos externos necesarios para la app
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
