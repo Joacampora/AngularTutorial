d4dd543a0d4344064b93908cbd98ef4ded91d711
Jonathan Campo <joacampora@unal.edu.co>
2018-03-02 19:30:49 -0500

    Displaying a List Completed

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from 'heroes/hero'
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Max' },
+  { id: 3, name: 'Zack' },
+  { id: 4, name: 'Lucy' },
+  { id: 5, name: 'Pauline' },
+  { id: 6, name: 'Angie' },
+  { id: 7, name: 'Eddy' },
+  { id: 8, name: 'Windy' },
+  { id: 9, name: 'Thorn' },
+  { id: 10, name: 'Magma' },
+  { id: 11, name: 'Silver' }
+];
